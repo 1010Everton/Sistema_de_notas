96d9016eb925d7b758048b803d4a5f9ec2e9ddac
EvErToN <evertonflorenciodasilva@outlook.com>
2025-05-20 13:18:20 -0300

    configurei os caminhos agora estou configurando a pesquisa

--- src/main/java/com/example/demo/controler/Pesquisa.java
+++ src/main/java/com/example/demo/controler/Pesquisa.java
@@ -1,13 +1,15 @@
 package com.example.demo.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestParam
+@RequestMapping("/pagina_principal")
 public class Pesquisa {
-
+    @GetMapping
     public String pesquisa (){
-
+    return "Nota_de_alunos";
     }
 }
